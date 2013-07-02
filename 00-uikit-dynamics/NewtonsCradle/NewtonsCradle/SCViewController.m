1cab94058152fef72124836cc55a5a52a58387bf
Sam Davies <sdavies@shinobicontrols.com>
2013-07-02 10:26:46 +0100

    Create project for UIDynamics
    We're going to build Newton's Cradle.

---
+++ 00-uikit-dynamics/NewtonsCradle/NewtonsCradle/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  NewtonsCradle
+//
+//  Created by Sam Davies on 02/07/2013.
+//  Copyright (c) 2013 ShinobiControls. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
