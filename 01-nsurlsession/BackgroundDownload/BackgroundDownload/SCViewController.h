1010bc344a7ecb972efd0bbdb7e0b6619d486369
Sam Davies <sdavies@shinobicontrols.com>
2013-07-04 21:03:32 +0100

    Creates BackgroundDownload application
    Just a template app for now.

---
+++ 01-nsurlsession/BackgroundDownload/BackgroundDownload/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  BackgroundDownload
+//
+//  Created by Sam Davies on 04/07/2013.
+//  Copyright (c) 2013 ShinobiControls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCViewController : UIViewController
+
+@end
