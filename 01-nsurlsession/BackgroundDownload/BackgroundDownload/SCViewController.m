a04c6c0a2ba02cac1005b128829786761fc6de0a
Sam Davies <sdavies@shinobicontrols.com>
2013-07-05 11:22:27 +0100

    Adds the ability to perform a resumable download.
    Need to find an image source that is actually resumable. At the moment
    it resumes from the partial data, but it doesn't include any payload...

--- 01-nsurlsession/BackgroundDownload/BackgroundDownload/SCViewController.m
+++ 01-nsurlsession/BackgroundDownload/BackgroundDownload/SCViewController.m
@@ -11,6 +11,7 @@
 @interface SCViewController () <NSURLSessionDownloadDelegate> {
     NSURLSession *inProcessSession;
     NSURLSessionDownloadTask *cancellableTask;
+    NSData *partialDownload;
 }
 
 @end
@@ -48,11 +49,34 @@
     if(cancellableTask) {
         [cancellableTask cancel];
         cancellableTask = nil;
+    } else if(self.resumableTask) {
+        [self.resumableTask cancelByProducingResumeData:^(NSData *resumeData) {
+            partialDownload = resumeData;
+            self.resumableTask = nil;
+        }];
     }
-    
 }
 
 - (IBAction)startResumable:(id)sender {
+    if(!self.resumableTask) {
+        if(!inProcessSession) {
+            // Create a 'private browsing' ses
+            NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+            inProcessSession = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:nil];
+            inProcessSession.sessionDescription = @"in-process NSURLSession";
+        }
+        
+        if(partialDownload) {
+            self.resumableTask = [inProcessSession downloadTaskWithResumeData:partialDownload];
+        } else {
+            NSString *url = @"http://hdnaturepictures.com/wp-content/uploads/2013/06/Mountains-View-Landscape.jpg";
+            NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+            self.resumableTask = [inProcessSession downloadTaskWithRequest:request];
+        }
+        
+        [self setDownloadButtonsAsEnabled:NO];
+        self.imageView.hidden = YES;
+    }
 }
 
 - (IBAction)startBackground:(id)sender {
@@ -69,13 +93,11 @@
 #pragma mark - NSURLSessionDownloadDelegate methods
 - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
 {
-    if(downloadTask == cancellableTask) {
-        double currentProgress = totalBytesWritten / (double)totalBytesExpectedToWrite;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.progressIndicator.hidden = NO;
-            self.progressIndicator.progress = currentProgress;
-        });
-    }
+    double currentProgress = totalBytesWritten / (double)totalBytesExpectedToWrite;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.progressIndicator.hidden = NO;
+        self.progressIndicator.progress = currentProgress;
+    });
 }
 
 - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes
@@ -85,35 +107,38 @@
 
 - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
 {
+    // We've successfully finished the download. Let's save the file
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSArray *URLs = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSURL *documentsDirectory = URLs[0];
+    
+    NSURL *destinationPath = [documentsDirectory URLByAppendingPathComponent:[location lastPathComponent]];
+    NSError *error;
+    
+    // Make sure we overwrite anything that's already there
+    [fileManager removeItemAtURL:destinationPath error:NULL];
+    BOOL success = [fileManager copyItemAtURL:location toURL:destinationPath error:&error];
+    
+    if (success)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage *image = [UIImage imageWithContentsOfFile:[destinationPath path]];
+            self.imageView.image = image;
+            self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+            self.imageView.hidden = NO;
+        });
+    }
+    else
+    {
+        NSLog(@"Couldn't copy the downloaded file");
+    }
+    
     if(downloadTask == cancellableTask) {
-        // We've successfully finished the download. Let's save the file
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        
-        NSArray *URLs = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
-        NSURL *documentsDirectory = URLs[0];
-        
-        NSURL *destinationPath = [documentsDirectory URLByAppendingPathComponent:[location lastPathComponent]];
-        NSError *error;
-        
-        // Make sure we overwrite anything that's already there
-        [fileManager removeItemAtURL:destinationPath error:NULL];
-        BOOL success = [fileManager copyItemAtURL:location toURL:destinationPath error:&error];
-        
-        if (success)
-        {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                UIImage *image = [UIImage imageWithContentsOfFile:[destinationPath path]];
-                self.imageView.image = image;
-                self.imageView.contentMode = UIViewContentModeScaleAspectFill;
-                self.imageView.hidden = NO;
-            });
-        }
-        else
-        {
-            NSLog(@"Couldn't copy the downloaded file");
-        }
-        
         cancellableTask = nil;
+    } else if (downloadTask == self.resumableTask) {
+        self.resumableTask = nil;
+        partialDownload = nil;
     }
 
 }
