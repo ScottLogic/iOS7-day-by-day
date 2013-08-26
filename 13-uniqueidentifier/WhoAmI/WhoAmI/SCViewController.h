a1a07345f1d941c8b6e63625e79c232aef396b81
Sam Davies <sdavies@shinobicontrols.com>
2013-08-26 17:22:51 +0100

    Completed simple app demonstrating unique identifiers in iOS7
    There's device and advertising.

--- 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.h
+++ 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SCViewController : UIViewController
+@interface SCViewController : UITableViewController
 
 @end
--- 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.m
+++ 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.m
@@ -7,10 +7,7 @@
 //
 
 #import "SCViewController.h"
-
-@interface SCViewController ()
-
-@end
+@import AdSupport;
 
 @implementation SCViewController
 
@@ -20,10 +17,80 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (NSUInteger)supportedInterfaceOrientations
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return UIInterfaceOrientationMaskLandscape;
 }
+
+#pragma mark - UITableViewDatasource methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return @"Vendor IDs";
+            break;
+            
+        case 1:
+            return @"Advertising IDs";
+            break;
+            
+        default:
+            return @"";
+            break;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+    
+    if(indexPath.section == 0) {
+        cell.textLabel.text = @"identifierForVendor";
+        cell.detailTextLabel.text = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    } else {
+        switch (indexPath.row) {
+            case 0:
+                cell.textLabel.text = @"advertisingIdentifier";
+                cell.detailTextLabel.text = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+                break;
+                
+            case 1:
+                cell.textLabel.text = @"isAdvertisingEnabled";
+                cell.detailTextLabel.text = [[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled] ? @"YES" : @"NO";
+                
+            default:
+                break;
+        }
+    }
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return 1;
+            break;
+            
+        case 1:
+            return 2;
+            break;
+            
+        default:
+            return 0;
+            break;
+    }
+}
+
+
 
 @end
