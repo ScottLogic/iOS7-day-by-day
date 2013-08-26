e48b956e0f640d76acb89f7b7665870eff4ee1a1
Sam Davies <sdavies@shinobicontrols.com>
2013-08-26 21:22:25 +0100

    Adds MAC address lookup

--- 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.m
+++ 13-uniqueidentifier/WhoAmI/WhoAmI/SCViewController.m
@@ -6,6 +6,11 @@
 //  Copyright (c) 2013 shinobicontrols. All rights reserved.
 //
 
+
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
 #import "SCViewController.h"
 @import AdSupport;
 
@@ -26,7 +31,7 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    return 3;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
@@ -38,6 +43,10 @@
             
         case 1:
             return @"Advertising IDs";
+            break;
+            
+        case 2:
+            return @"Networking";
             break;
             
         default:
@@ -56,7 +65,7 @@
     if(indexPath.section == 0) {
         cell.textLabel.text = @"identifierForVendor";
         cell.detailTextLabel.text = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
-    } else {
+    } else if (indexPath.section == 1) {
         switch (indexPath.row) {
             case 0:
                 cell.textLabel.text = @"advertisingIdentifier";
@@ -70,6 +79,9 @@
             default:
                 break;
         }
+    } else {
+        cell.textLabel.text = @"MAC Adddress";
+        cell.detailTextLabel.text = [self getMacAddress];
     }
     return cell;
 }
@@ -84,6 +96,10 @@
         case 1:
             return 2;
             break;
+        
+        case 2:
+            return 1;
+            break;
             
         default:
             return 0;
@@ -91,6 +107,60 @@
     }
 }
 
+
+- (NSString *)getMacAddress
+{
+    int                 mgmtInfoBase[6];
+    char                *msgBuffer = NULL;
+    NSString            *errorFlag = NULL;
+    size_t              length;
+    
+    // Setup the management Information Base (mib)
+    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
+    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
+    mgmtInfoBase[2] = 0;
+    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
+    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
+    
+    // With all configured interfaces requested, get handle index
+    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0)
+        errorFlag = @"if_nametoindex failure";
+    // Get the size of the data available (store in len)
+    else if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0)
+        errorFlag = @"sysctl mgmtInfoBase failure";
+    // Alloc memory based on above call
+    else if ((msgBuffer = malloc(length)) == NULL)
+        errorFlag = @"buffer allocation failure";
+    // Get system information, store in buffer
+    else if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0)
+    {
+        free(msgBuffer);
+        errorFlag = @"sysctl msgBuffer failure";
+    }
+    else
+    {
+        // Map msgbuffer to interface message structure
+        struct if_msghdr *interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
+        
+        // Map to link-level socket structure
+        struct sockaddr_dl *socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
+        
+        // Copy link layer address data in socket structure to an array
+        unsigned char macAddress[6];
+        memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
+        
+        // Read from char array into a string object, into traditional Mac address format
+        NSString *macAddressString = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+                                      macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]];
+        
+        // Release the buffer memory
+        free(msgBuffer);
+        
+        return macAddressString;
+    }
+    
+    return errorFlag;
+}
 
 
 @end
