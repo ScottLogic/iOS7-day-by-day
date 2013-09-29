4c6bd41804b16804beb4f72f254ee23af4605524
Sam Davies <sdavies@shinobicontrols.com>
2013-09-29 21:23:48 +1000

    Create initial project to demonstrate the new snapshotting methods

---
+++ 07-snapshot/ViewSnapper/ViewSnapper/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  ViewSnapper
+//
+//  Created by Sam Davies on 29/09/2013.
+//  Copyright (c) 2013 shinobicontrols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 07-snapshot/ViewSnapper/ViewSnapper/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  ViewSnapper
+//
+//  Created by Sam Davies on 29/09/2013.
+//  Copyright (c) 2013 shinobicontrols. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
