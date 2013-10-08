cb6c3f7f5e7a6f8f09be419e002f26dfae1e637e
Sam Davies <sdavies@shinobicontrols.com>
2013-10-08 16:13:13 +0100

    Create a sample application to demonstrate this post

---
+++ 16-qr-codes-avfoundation/CodeScanner/CodeScanner/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  CodeScanner
+//
+//  Created by Sam Davies on 08/10/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCViewController : UIViewController
+
+@end
---
+++ 16-qr-codes-avfoundation/CodeScanner/CodeScanner/SCViewController.m
@@ -0,0 +1,28 @@
+//
+//  SCViewController.m
+//  CodeScanner
+//
+//  Created by Sam Davies on 08/10/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+@end
