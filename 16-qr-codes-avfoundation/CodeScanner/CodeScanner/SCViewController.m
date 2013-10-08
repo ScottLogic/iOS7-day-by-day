21ffe0e99f3e02db8013a0fbfeeafe55f5b7e436
Sam Davies <sdavies@shinobicontrols.com>
2013-10-08 21:41:28 +0100

    Got the basics of a QR reader working
    Need to tidy up the code a bit, and add the drawing of the corners, but
    it's mostly there :)

--- 16-qr-codes-avfoundation/CodeScanner/CodeScanner/SCViewController.m
+++ 16-qr-codes-avfoundation/CodeScanner/CodeScanner/SCViewController.m
@@ -9,7 +9,12 @@
 #import "SCViewController.h"
 @import AVFoundation;
 
-@interface SCViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@interface SCViewController () <AVCaptureMetadataOutputObjectsDelegate> {
+    AVCaptureVideoPreviewLayer *_previewLayer;
+    UIView *_boundingBox;
+    NSTimer *_boxHideTimer;
+    UILabel *_decodedMessage;
+}
 
 @end
 
@@ -33,11 +38,35 @@
     }
     
     AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
+    [session addOutput:output];
+    //NSLog(@"%@",[output availableMetadataObjectTypes]);
     [output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
     [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    [session addOutput:output];
+    
+    
+    // Display on screen
+    _previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    _previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _previewLayer.bounds = self.view.bounds;
+    _previewLayer.position = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+    [self.view.layer addSublayer:_previewLayer];
     
     [session startRunning];
+    
+    _boundingBox = [[UIView alloc] initWithFrame:self.view.bounds];
+    _boundingBox.layer.borderColor = [[UIColor redColor] CGColor];
+    _boundingBox.layer.borderWidth = 2.0;
+    _boundingBox.backgroundColor = [UIColor clearColor];
+    _boundingBox.hidden = YES;
+    [self.view addSubview:_boundingBox];
+    
+    _decodedMessage = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.bounds) - 75, CGRectGetWidth(self.view.bounds), 75)];
+    _decodedMessage.numberOfLines = 0;
+    _decodedMessage.backgroundColor = [UIColor colorWithWhite:0.8 alpha:0.9];
+    _decodedMessage.textColor = [UIColor darkGrayColor];
+    _decodedMessage.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_decodedMessage];
+    
 }
 
 - (NSUInteger)supportedInterfaceOrientations
@@ -52,11 +81,37 @@
     NSString *qrCode = nil;
     for (AVMetadataObject *metadata in metadataObjects) {
         if ([metadata.type isEqualToString:AVMetadataObjectTypeQRCode]) {
-            qrCode = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
-            break;
+            AVMetadataObject *transformed = [_previewLayer transformedMetadataObjectForMetadataObject:metadata];
+            _boundingBox.frame = transformed.bounds;
+            _boundingBox.hidden = NO;
+            [self startTimer];
+            
+            qrCode = [(AVMetadataMachineReadableCodeObject *)transformed stringValue];
+            
+            if(_decodedMessage.text != qrCode) {
+                _decodedMessage.text = qrCode;
+            }
         }
     }
-    NSLog(@"QRCode: %@", qrCode);
+}
+
+- (void)startTimer
+{
+    if(_boxHideTimer) {
+        [_boxHideTimer invalidate];
+    }
+    
+    _boxHideTimer = [NSTimer scheduledTimerWithTimeInterval:0.2
+                                                     target:self
+                                                   selector:@selector(removeBoundingBox:)
+                                                   userInfo:nil
+                                                    repeats:NO];
+}
+
+- (void)removeBoundingBox:(id)sender
+{
+    _boundingBox.hidden = YES;
+    _decodedMessage.text = @"";
 }
 
 
