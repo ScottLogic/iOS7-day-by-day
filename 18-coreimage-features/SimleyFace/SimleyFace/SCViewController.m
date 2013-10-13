eae4023547bc526f859a03da5a2e56b21d3d1ad8
Sam Davies <sdavies@shinobicontrols.com>
2013-10-13 16:52:44 +0100

    Couple of bug-fixes and correct license headers

--- 18-coreimage-features/SimleyFace/SimleyFace/SCViewController.m
+++ 18-coreimage-features/SimleyFace/SimleyFace/SCViewController.m
@@ -1,10 +1,19 @@
-//
-//  SCViewController.m
-//  SimleyFace
-//
-//  Created by Sam Davies on 13/10/2013.
-//  Copyright (c) 2013 shinobicontrols. All rights reserved.
-//
+/*
+ Copyright 2013 Scott Logic Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 
 #import "SCViewController.h"
 @import AVFoundation;
@@ -49,7 +58,7 @@
     [_session addOutput:output];
     // What different things can we register to recognise?
     NSLog(@"%@", [output availableMetadataObjectTypes]);
-    // We're only interested in QR Codes
+    // We're only interested in faces
     [output setMetadataObjectTypes:@[AVMetadataObjectTypeFace]];
     // This VC is the delegate. Please call us on the main queue
     [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
@@ -146,7 +155,7 @@
                                                    options:@{CIDetectorEyeBlink: @YES,
                                                              CIDetectorSmile: @YES,
                                                              CIDetectorImageOrientation: @5}];
-        NSLog(@"%d features", [features count]);
+        NSLog(@"%lu features", [features count]);
         BOOL happyPicture = NO;
         if([features count] > 0) {
             happyPicture = YES;
