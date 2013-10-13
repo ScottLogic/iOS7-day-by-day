dcb69e0cf111c2dc988bc29b538e97c1765b27b2
Sam Davies <sdavies@shinobicontrols.com>
2013-10-13 09:26:27 +0100

    Creates empty app for CoreImage face detection demo

---
+++ 18-coreimage-features/SimleyFace/SimleyFace/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  SimleyFace
+//
+//  Created by Sam Davies on 13/10/2013.
+//  Copyright (c) 2013 shinobicontrols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCViewController : UIViewController
+
+@end
